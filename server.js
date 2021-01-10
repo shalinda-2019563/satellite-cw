31db5d352cceceb536942ca81f34e47721fed3ed
shalinda-2019563 <75839960+shalinda-2019563@users.noreply.github.com>
2021-01-10 23:45:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,70 +13,16 @@ const app = express();
 app.use(bodyParser.json())
 
 
-app.get('/sat-1/', (req, res) => {
-  res.send('Welcome to Satellite 1');
+app.get('/sat-1/hello', (req, res) => {
+  res.send('Hello satellite 1');
 });
 
-app.get('/sat-2/', (req, res) => {
-  res.send('Welcome to Satellite 2');
+app.get('/sat-2/hello', (req, res) => {
+  res.send('Hello satellite 2');
 });
 
 
-app.get('/sat-1/position', (req, res) => {
-
-  //Kubernetis
-  //   var sequelize = new Sequelize('my_db', 'root', '', {
-  //     host: 'mysql',
-  //     port: 3306,
-  //     dialect: 'mysql'
-  // });
-
-  mysql.createConnection({
-    host: 'mysql',
-    port: 3306,
-    user: 'root',
-    password: ''
-  }).then((connection) => {
-
-
-    connection.query('CREATE DATABASE IF NOT EXISTS satellite_db;').then(() => {
-      connection.query('CREATE TABLE IF NOT EXISTS `satellite_db`.`satellites` ( `id` INT NOT NULL AUTO_INCREMENT , `latitude` VARCHAR(45) NOT NULL , `longitude` VARCHAR(45) NOT NULL , PRIMARY KEY (`id`));').then(() => {
-
-        const lat = Math.round((Math.random() * 360 - 180) * 1000) / 1000
-        const long = Math.round((Math.random() * 360 - 180) * 1000) / 1000
-
-        connection.query(`INSERT INTO satellite_db.satellites (latitude,longitude) VALUES(${lat},${long});`).then(() => {
-
-
-
-          res.send(`<h1>Satellite is in position of latitude ${lat} and longitude of ${long} </h1>`);
-
-          //Insert Query
-        }).catch(function (err) {
-          console.log("TABLE Insert FAILED");
-          console.log(err)
-          res.send(err);
-        });
-
-
-        //Table
-      }).catch(function (err) {
-        console.log("TABLE CREATION FAILED");
-        console.log(err)
-        res.send(err);
-      });
-
-      //Database
-    }).catch(function (err) {
-      console.log("DATABASE CREATION FAILED");
-      console.log(err)
-      res.send(err);
-    });
-
-  })
-  
-  
-  app.get('/sat-2/position', (req, res) => {
+app.get('/position', (req, res) => {
 
   //Kubernetis
   //   var sequelize = new Sequelize('my_db', 'root', '', {

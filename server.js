ade939e137fbe3acac2e9d2893cd0eee409b9faa
shalinda-2019563 <75839960+shalinda-2019563@users.noreply.github.com>
2021-01-31 11:36:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello satellite');
+  res.send('Hello satellite world');
 });
 
 app.get('/sat-1/', (req, res) => {

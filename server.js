6cf69d45caeac90ace48bed0a1a5220f2f1b7eb8
Shalinda <shalinda@arimaclanka.com>
2021-01-31 18:12:45 +0530

    Update server.js
    update response text

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello satellite world');
+  res.send('System is healthy');
 });
 
 app.get('/sat-1/', (req, res) => {
